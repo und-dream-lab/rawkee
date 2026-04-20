e7c38dd6fea32547bcb73b32b7c3ea05827f4fb6
Thomaz Díaz <thomas.diaz@und.edu>
2026-04-20 14:39:45 -0500

    UI Side Bar Panels

--- Blender_RawKee_Python_X3D.py
+++ Blender_RawKee_Python_X3D.py
@@ -6,7 +6,7 @@ Description:
     Contains the Plugin Information for Blender,
     and the file imports for running the plugin. 
 
-    & Also at the moment adds the export drop down in the File>Export
+    & Also at the moment adds the export drop down in the File>Export & UI panel
 
 File Status:
     (In Development)
@@ -14,7 +14,6 @@ File Status:
         Learn about the Blender Node Scene Graph
 """
 #Blender Plugin Information 
-
 bl_info = {
     "name" : "Blender RawKee X3D Export Plugin",
     "author" : "UND Dream Lab, etc...", 
@@ -29,30 +28,110 @@ bl_info = {
 #Blender Imports
 import bpy
 
-#Adds the File>Export>RawKee X3D (.x3d) and File>Import>RawKee X3D (.x3d) dropdowns
+#-------------------------------------
+#menu panel UI. (RKWeb3D) (no functionality added yet.)
+class RKMainPanel(bpy.types.Panel):
+    bl_label = "RawKee (.X3D)"
+    bl_idname = "PT_RAWKEE_Main_Panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'RawKee (.X3D)'
+
+    #draws the contents of the side bar panal.
+    def draw(self, context):
+        layout = self.layout
+        rowPanel = layout.row() #short cut to make rows.
+
+#X3D Interaction Editor
+class RKSubPanel1(bpy.types.Panel):
+    bl_label = "X3D Interaction Editor"
+    bl_idname = "PT_RAWKEE_Sub_Panel1"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'RawKee (.X3D)'
+    bl_parent_id = 'PT_RAWKEE_Main_Panel'
+
+    def draw(self,context):
+        layout = self.layout
+        rowPanel = layout.row()
+
+        rowPanel.label(text = 'controls/pop-up windows here')
+
+#X3D Character and Animation Editor
+class RKSubPanel2(bpy.types.Panel):
+    bl_label = "X3D Character and Animation Editor"
+    bl_idname = "PT_RAWKEE_Sub_Panel2"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'RawKee (.X3D)'
+    bl_parent_id = 'PT_RAWKEE_Main_Panel'
+
+    def draw(self,context):
+        layout = self.layout
+        rowPanel = layout.row()
+
+        rowPanel.label(text = 'controls/pop-up windows here')
+
+#X3D General Animation Editor
+class RKSubPanel3(bpy.types.Panel):
+    bl_label = "X3D Character and Animation Editor"
+    bl_idname = "PT_RAWKEE_Sub_Panel3"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'RawKee (.X3D)'
+    bl_parent_id = 'PT_RAWKEE_Main_Panel'
+
+    def draw(self,context):
+        layout = self.layout
+        rowPanel = layout.row()
+
+        rowPanel.label(text = 'controls/pop-up windows here')
+
+#-------------------------------------
+#File>Export dropdown UI. (RKWeb3D) (no functionality added yet.)
 class X3D_FileDropDown(bpy.types.Operator):
     bl_idname = "rawkee_blender_scene_export.x3d"
     bl_label = "RawKee X3D (.x3d)"
     bl_description = "Exports scene as X3D file"
 
-#process when the export is exicuted.
+#process when the export is executed.
     def execute(self,context):
         #Nothing is happening here at the moment. <===== left off here, - Thomaz Diaz (April 14th 2026)
 
         return {'FINISHED'}
 
-#Blender options.
+#-------------------------------------
+#
 def menu_func_export(self, context):
     self.layout.operator(X3D_FileDropDown.bl_idname, text="RawKee X3D (.x3d)")
-    
-#Blender Register and Unregister
+
+
+
+
+#-------------------------------------  
+# register and unregister
 def register():
     bpy.utils.register_class(X3D_FileDropDown)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
-    # 
+    #-------------------------------------
+    bpy.utils.register_class(RKMainPanel)
+
+    bpy.utils.register_class(RKSubPanel1)
+    bpy.utils.register_class(RKSubPanel2)
+    bpy.utils.register_class(RKSubPanel3)
+    #-------------------------------------
+
 def unregister():
-    #
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
     bpy.utils.unregister_class(X3D_FileDropDown)  
+    #-------------------------------------
+    bpy.utils.unregister_class(RKMainPanel)
+
+    bpy.utils.unregister_class(RKSubPanel1)
+    bpy.utils.unregister_class(RKSubPanel2)
+    bpy.utils.unregister_class(RKSubPanel3)
+    #-------------------------------------
+
+
 if __name__ == "__main__":
      register()
